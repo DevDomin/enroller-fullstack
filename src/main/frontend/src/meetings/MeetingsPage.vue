8fbe40f041e5f23e0df7278dede497d4e15484f8
Dominik <devdomin@gmail.com>
2019-05-19 23:05:00 +0200

    Add files via upload

--- src/main/frontend/src/meetings/MeetingsPage.vue
+++ src/main/frontend/src/meetings/MeetingsPage.vue
@@ -2,7 +2,7 @@
   <div>
     <new-meeting-form @added="addNewMeeting($event)"></new-meeting-form>
 
-    <span v-if="meetings.length == 0">
+    <span v-if="meetings.length === 0">
                Brak zaplanowanych spotkań.
            </span>
     <h3 v-else>
@@ -30,18 +30,53 @@
             };
         },
         methods: {
+            getMeetings() {
+                this.$http.get('meetings')
+                    .then(response => { 
+                        this.meetings = response.body;
+                    })
+            },
             addNewMeeting(meeting) {
-                this.meetings.push(meeting);
+                this.$http.post('meetings', meeting)
+                    .then(response => {
+                        this.meetings.push(response.body);
+                    });
+                this.getMeetings();
             },
             addMeetingParticipant(meeting) {
-                meeting.participants.push(this.username);
+                  meeting.participants.push(this.username);
+                this.$http.post('meetings/'+ meeting.id +'/participants', {login:this.username})
+                     .then(response => {
+                         console.log("udało się zapisac");
+                          this.getMeetings();
+                     })
+                     .catch(response => {
+                          console.log("nie udało się zapisac");
+                     });
             },
             removeMeetingParticipant(meeting) {
                 meeting.participants.splice(meeting.participants.indexOf(this.username), 1);
+                
+                this.$http.delete('meetings/'+ meeting.id + '/participants/' + this.username)
+                     .then(response => {
+                         console.log("udało się zapisac");
+                          this.getMeetings();
+                     })
+                     .catch(response => {
+                          console.log("nie udało się zapisac");
+                     });
             },
             deleteMeeting(meeting) {
+                this.$http.delete('meetings/' + meeting.id.toString())
+                    .then(response =>{
+                        this.meetings.splice(this.meetings.indexOf(meeting), 1);
+                    });
                 this.meetings.splice(this.meetings.indexOf(meeting), 1);
+                this.getMeetings();
             }
+        },
+        mounted() {
+            this.$nextTick(this.getMeetings());
         }
     }
 </script>
